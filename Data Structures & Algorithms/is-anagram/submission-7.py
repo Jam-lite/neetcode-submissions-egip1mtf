9aa8883e6873c6032875fb73ddde0d84879ac1c7
Jamie Johnston <123163336+Jam-lite@users.noreply.github.com>
2026-08-24 09:02:36 +0100

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        char_freq_s, char_freq_t = {},{}
+
+        for char in s:
+            char_freq_s[char] = 1 + char_freq_s.get(char, 0)
+        
+        for char in t:
+            char_freq_t[char] = 1 + char_freq_t.get(char, 0)
+        
+        if char_freq_s == char_freq_t:
+            return True
+        
+        return False
+
+        
